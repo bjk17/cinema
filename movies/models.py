2dbef7f137f68bcda39c4af0394ce1bc499d591c
Bjarni Jens Kristinsson <bjarni.jens@gmail.com>
2014-11-18 16:51:44 +0000

    ORM draft

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    released = models.IntegerField()
+    restricted = models.IntegerField()
+    imdb = models.CharField(max_length=20)
+    image = models.URLField(max_length=30)
+    #~ question_text = models.CharField(max_length=200)
+    #~ pub_date = models.DateTimeField('date published')
+
+
+class Showtime(models.Model):
+    movie = models.ForeignKey(Movie)
+    cinema = models.CharField(max_length=20)
+    time = models.CharField(max_length=20)
+    #~ choice_text = models.CharField(max_length=200)
+    #~ votes = models.IntegerField(default=0)
