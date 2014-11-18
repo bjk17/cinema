b928bd9cdbab691c4dc024c463fbbf5669df0b00
Bjarni Jens Kristinsson <bjarni.jens@gmail.com>
2014-11-18 13:53:16 +0000

    Prufu URL

--- movies/urls.py
+++ movies/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
-
+from movies import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Vefforritun.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^prufa/', include('movies.urls')),
+    url(r'^$', views.index, name='index'),
 )
