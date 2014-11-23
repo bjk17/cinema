951c526bb32662231a65774190b4677ce7e17c53
Bjarni Jens Kristinsson <bjarni.jens@gmail.com>
2014-11-23 23:45:39 +0000

    basic uuid creation and sample at /wm

--- movies/models.py
+++ movies/models.py
@@ -2,14 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    title = models.CharField(max_length=50)
-    released = models.IntegerField()
+    title = models.CharField(max_length=50, primary_key=True)
+    released = models.IntegerField(primary_key=True)
     restricted = models.IntegerField()
     imdb = models.CharField(max_length=20)
     image = models.URLField(max_length=30)
-
+    
+    unique_together = ("title", "released")
+    
     def __unicode__(self):
-    	return u'%s (%s)' % (self.title, self.released)
+        return u'%s (%s)' % (self.title, self.released)
 
 class Showtime(models.Model):
     movie = models.ForeignKey(Movie)
@@ -17,7 +19,7 @@ class Showtime(models.Model):
     time = models.CharField(max_length=20)
 
     def __unicode__(self):
-    	return u'%s %s' % (self.cinema, self.time)
+        return u'%s %s' % (self.cinema, self.time)
 
 class Timestamp(models.Model):
     timeFetched = models.FloatField()
