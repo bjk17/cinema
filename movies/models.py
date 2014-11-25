49216261a78c3eb7c75f742c58129b0d1b01b4f4
Kristrún Skúladóttir <krs65@hi.is>
2014-11-25 13:23:32 +0000

    Stylingg

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
@@ -17,9 +18,19 @@ class Showtime(models.Model):
     movie = models.ForeignKey(Movie)
     cinema = models.CharField(max_length=50)
     time = models.CharField(max_length=20)
+     
+    def getCinema(self):
+        reps = {u'á':'a', u'ð':'d', u'é':'e', u'í':'i', u'ó':'o', u'ú':'u', u'ý':'y', u'þ':'th', u'æ':'ae', u'ö':'o'}
+        cinema = replace_all(self.cinema,reps)
+        return u'%s' % (cinema,)
 
     def __unicode__(self):
         return u'%s %s' % (self.cinema, self.time)
 
 class Timestamp(models.Model):
     timeFetched = models.FloatField()
+
+def replace_all(text, dic):
+    for i, j in dic.iteritems():
+        text = text.replace(i, j)
+    return text
