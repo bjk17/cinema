e5c4aaf6ec259b52b336554b2d0d332cfe470eec
gudrunthora <gudrunthora@yahoo.com>
2014-11-25 15:01:18 +0000

    small changes

--- movies/static/movies/script.js
+++ movies/static/movies/script.js
@@ -1,30 +1,19 @@
-/*$( document ).ready(function() {
+$( document ).ready(function() {
 //eitthvað hér? ef ekki þá eyða út
 });
 
-
-$("div#chooseTheater").click(function() {
-	$( "ul#theaterList" ).toggleClass( "hidden" );
+$("#chooseTheater").on("click", function() {
+	$( "#theaterList" ).toggleClass( "hidden" );
 });
 
-$(".showShowtimes").click(function() {
-	alert("yess");
-    var element = $(this).closest('.movie').detach();
-    alert(element);
-    if 
-    $('#movies').append(element);
-});*/
-
 $( document ).on("click", "#movies .movie .showShowtimes", function() {
-	console.log("movies");
-    var element = $(this).closest('.movie').detach();
-    $('#toSee').append(element);
+    var element = $(this).closest(".movie").detach();
+    $("#toSee").append(element);
 });
 
 $( document ).on("click", "#toSee .movie .showShowtimes", function() {
-	console.log("toSee");
     var element = $(this).closest('.movie').detach();
-    $('#movies').append(element);
+    $("#movies").append(element);
 });
 
 
